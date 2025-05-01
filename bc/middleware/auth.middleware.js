762566857ec6e9357c1e39c93a758d5a4e30d3fe
Akshai-M <mylapuramakshaikumar@gmail.com>
2025-05-01 10:20:53 +0530

    authniddle

--- bc/middleware/auth.middleware.js
+++ bc/middleware/auth.middleware.js
@@ -3,7 +3,7 @@ import User from "../models/user.model.js";
 
 const protectRoute = async (req, res, next) => {
   try {
-    const token = req.cookie.jwt;
+    const token = req.cookies.jwt;
 
     if (!token)
       return res
@@ -13,7 +13,7 @@ const protectRoute = async (req, res, next) => {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     if (!decoded) return res.status(400).json("Invalid token");
 
-    const user = await User.findOne(decoded.userId).select("-password");
+    const user = await User.findById(decoded.userId).select("-password");
 
     if (!user) return res.status(404).json({ message: "User not found" });
 
